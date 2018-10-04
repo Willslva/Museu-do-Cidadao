9d6d67c92a3211d529d3f39845d2b5da018b12de
baskerville2018 <megatronstall@gmail.com>
2018-10-04 17:56:39 -0300

    My Django Girls app, first commit

--- static/js/exposicao.js
+++ static/js/exposicao.js
@@ -1,18 +1,31 @@
 function validar() {
   form1 = document.getElementById("form");
   var nome = form1.username.value;
+  var first_name = form1.first_name.value;
+  var last_name = form1.last_name.value;
   var email = form1.email.value;
+  var password = form1.password.value;
 
     if (nome == "") {
 alert('Preencha o campo com seu nome');
 nome.focus();
 return false;
 }
-// if (password.length < 8) {
-// alert('Preencha o campo com sua senha com no mímino 8 caracteres');
-// password.focus();
-// return false;
-// }
+if (first_name == "") {
+alert('Preencha o campo com seu primeiro nome');
+first_name.focus();
+return false;
+}
+if (last_name == "") {
+alert('Preencha o campo com seu último nome');
+last_name.focus();
+return false;
+}
+if (password.length < 8) {
+    alert('Preencha o campo com sua senha com no mímino 8 caracteres');
+    password.focus();
+    return false;
+  }
 
 if(email.indexOf("@") == -1 ||
       email.indexOf(".") == -1 ||
@@ -22,4 +35,49 @@ if(email.indexOf("@") == -1 ||
         email.focus();
         return false;
     }
+}
+
+function auth() {
+  form1 = document.getElementById("formauth");
+  var nome = form1.username.value;
+  var password = form1.password.value;
+
+    if (nome == "") {
+      alert('Preencha o campo com seu nome');
+      nome.focus();
+      return false;
+}
+
+if (password.length < 8) {
+    alert('Preencha o campo com sua senha com no mímino 8 caracteres');
+    password.focus();
+    return false;
+  }
+
+}
+
+function exposicao() {
+  form1 = document.getElementById("formexposicao");
+  var titulo = form1.titulo.value;
+  var assunto = form1.assunto.value;
+  var original = form1.original.value;
+
+    if (titulo == "") {
+      alert('Preencha o campo com o titulo de sua exposição');
+      titulo.focus();
+      return false;
+}
+
+ if (assunto == "") {
+      alert('Preencha o campo com o assunto da sua exposição');
+      assunto.focus();
+      return false;
+}
+
+if (original == "") {
+    alert('Adicione alguma foto na sua exposição');
+    original.focus();
+    return false;
+  }
+
 }
